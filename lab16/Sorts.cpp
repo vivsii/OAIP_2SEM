f49dacfdcd98281cc09560c2a91c3f32942ed128
vivsii <116980278+vivsii@users.noreply.github.com>
2023-07-20 01:04:58 +0300

    Create Sorts.cpp

---
+++ lab16/Sorts.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+void bubbleSort(int a[], int n)
+{
+	int i, j, t;
+	for (i = 1; i < n; i++)
+		for (j = n - 1; j >= i; j--)
+			if (a[j - 1] < a[j])
+			{
+				t = a[j - 1];
+				a[j - 1] = a[j];
+				a[j] = t;
+			}
+}
+
+int getHoarBorder(int A[], int sm, int em)
+{
+	int i = sm - 1, j = em + 1;
+	int brd = A[sm];
+	int buf;
+	while (i < j)
+	{
+		while (A[--j] < brd);
+		while (A[++i] > brd);
+		if (i > j)
+		{
+			buf = A[j];
+			A[j] = A[i];
+			A[i] = buf;
+		};
+	}
+	return j;
+}
+int* sortHoar(int A[], int sm, int em)
+{
+	if (sm < em)
+	{
+		int hb = getHoarBorder(A, sm, em);
+		sortHoar(A, hb + 1, em);
+		sortHoar(A, sm, hb);
+	}
+	return A;
+};
